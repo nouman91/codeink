1c7afb2d720611aff46d2c66484b1d41121ad355
Nouman.Waheed <nouman.whaeed@nmxglobalsoftware.com>
2019-10-07 16:58:21 +0500

    file formatting

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,7 +4,7 @@ const { createFilePath } = require(`gatsby-source-filesystem`)
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
 
-  const blogPost = path.resolve(`./src/templates/blog-post.js`);
+  const blogPost = path.resolve(`./src/templates/blog-post.js`)
   const pageTemplate = path.resolve(`./src/templates/page.js`)
   const result = await graphql(
     `
@@ -36,31 +36,28 @@ exports.createPages = async ({ graphql, actions }) => {
   // Create blog posts pages.
   const posts = result.data.allMarkdownRemark.edges
   posts.forEach((post, index) => {
-    if(post.node.frontmatter.layout==="page"){
-      console.log("..................................")
+    if (post.node.frontmatter.layout === "page") {
       createPage({
         path: post.node.fields.slug,
         component: pageTemplate,
         context: {
-          slug: post.node.fields.slug
-        }
+          slug: post.node.fields.slug,
+        },
+      })
+    } else {
+      const previous = index === posts.length - 1 ? null : posts[index + 1].node
+      const next = index === 0 ? null : posts[index - 1].node
+
+      createPage({
+        path: post.node.fields.slug,
+        component: blogPost,
+        context: {
+          slug: post.node.fields.slug,
+          previous,
+          next,
+        },
       })
     }
-    else{
-      const previous = index === posts.length - 1 ? null : posts[index + 1].node
-    const next = index === 0 ? null : posts[index - 1].node
-
-    createPage({
-      path: post.node.fields.slug,
-      component: blogPost,
-      context: {
-        slug: post.node.fields.slug,
-        previous,
-        next,
-      },
-    })
-    }
-    
   })
 }
 
@@ -69,7 +66,7 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
 
   if (node.internal.type === `MarkdownRemark`) {
     const value = createFilePath({ node, getNode })
-    console.log(value);
+    console.log(value)
     createNodeField({
       name: `slug`,
       node,
