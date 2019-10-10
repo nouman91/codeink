28ce6bfd6f480db8657bd61c1ffecc763b5068ef
Nouman.Waheed <nouman.whaeed@nmxglobalsoftware.com>
2019-10-10 15:47:57 +0500

    syntax code highlighting

--- gatsby-config.js
+++ gatsby-config.js
@@ -49,6 +49,7 @@ module.exports = {
               wrapperStyle: `margin-bottom: 1.0725rem`,
             },
           },
+          `gatsby-remark-vscode`,
           `gatsby-remark-prismjs`,
           `gatsby-remark-copy-linked-files`,
           `gatsby-remark-smartypants`,
