7b3e5e9f97c1caad5e7956da7b203b58c9c7df39
Nouman.Waheed <nouman.whaeed@nmxglobalsoftware.com>
2019-12-08 11:27:10 +0500

    committing favicon change

--- gatsby-config.js
+++ gatsby-config.js
@@ -75,7 +75,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/fav.png`,
+        icon: `content/assets/favicon.jpg`,
       },
     },
     `gatsby-plugin-offline`,
